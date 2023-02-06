d320400d542925bf1812c2cd74f03c629caa7e78
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-06 13:23:58 -0300

    feat: Session and user schema]

---
+++ src/schema/user/index.ts
@@ -0,0 +1,68 @@
+import * as yup from "yup";
+import { IUserRequest, IUserUpdate } from "../../interfaces/user";
+
+
+export const userCreate: yup.SchemaOf<IUserRequest> = yup.object().shape({
+    name: yup.string().required(),
+    password: yup.string().required(),
+    email: yup.string().email().required(),
+    telephone: yup.string().required()
+});
+
+export const userUpdate: yup.SchemaOf<IUserUpdate> = yup.object().shape(
+    {
+        email: yup
+            .string()
+            .email()
+            .when(["name", "password", "telephone"], {
+                is: (name: string, password: string) => {
+                    if (name || password) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                then: (schema) => schema.notRequired(),
+                otherwise: (schema) => schema.required(),
+            }),
+        name: yup.string().when(["email", "password"], {
+            is: (email: string, password: string) => {
+                if (email || password) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            then: (schema) => schema.notRequired(),
+            otherwise: (schema) => schema.required(),
+        }),
+        password: yup.string().when(["email", "name"], {
+            is: (email: string, name: string) => {
+                if (email || name) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            then: (schema) => schema.notRequired(),
+            otherwise: (schema) => schema.required(),
+        }),
+        telephone: yup.string().when(["email", "name"], {
+            is: (email: string, name: string) => {
+                if (email || name) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            then: (schema) => schema.notRequired(),
+            otherwise: (schema) => schema.required(),
+        }),
+    },
+    [
+        ["name", "password"],
+        ["email", "password"],
+        ["email", "name"],
+        ["email", "name"],
+    ]
+);
