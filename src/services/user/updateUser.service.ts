89d10770903327160f0d4e7c636c0838d62e33fb
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-06 15:16:06 -0300

    feat: User services

--- src/services/user/updateUser.service.ts
+++ src/services/user/updateUser.service.ts
@@ -4,27 +4,23 @@ import { AppDataSource } from "../../data-source"
 import { User } from "../../entities/user.entity"
 import { IUserUpdate } from "../../interfaces/user"
 
-export const updateUserService = async (
-    { name, email, password, telephone }: IUserUpdate,
-    id: string
-) => {
+export const updateUserService = async ({ name, email, password, telephone }: IUserUpdate, id: string) => {
     const userRepository = AppDataSource.getRepository(User)
 
-    const findUser = await userRepository.findOneBy({
-        id,
-    })
+    const findUser = await userRepository.findOneBy({ id })
 
     if (!findUser) {
         throw new AppError("User is not found", 404)
     }
 
-    const user = await userRepository.update(id, {
+    userRepository.update(findUser!.id, {
         name: name ? name : findUser!.name,
         email: email ? email : findUser!.email,
         password: password ? hashSync(password, 10) : findUser!.password,
         telephone: telephone ? telephone : findUser!.telephone,
+        updatedAt: new Date()
     })
 
-    return user!
+    return true
 }
 
