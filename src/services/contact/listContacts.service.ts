607e3c7f1cfccc427fd1deac6d01ad5f2a00d68a
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-06 19:10:22 -0300

    feat: Contact services

--- src/services/contact/listContacts.service.ts
+++ src/services/contact/listContacts.service.ts
@@ -1,18 +1,16 @@
-import { AppError } from "../../errors/appError"
 import { AppDataSource } from "../../data-source"
 import { Contact } from "../../entities/contact.entity"
+import { User } from "../../entities/user.entity"
 
 export const listContactService = async (id: string) => {
-    const contactRepository = AppDataSource.getRepository(Contact)
+    const userRepository = AppDataSource.getRepository(User)
 
-    const contacts = await contactRepository.findOne({
+    const contacts = await userRepository.findOne({
         where: { id },
+        relations: {
+            contacts: true
+        }
     })
 
-    if (!contacts) {
-        throw new AppError("Contact does not exist", 404)
-    }
-
-    return contacts;
-};
-
+    return contacts?.contacts;
+}
