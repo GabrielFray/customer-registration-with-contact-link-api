57e938a2cf231bc88df22f2bd2f128ef83657a32
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-07 15:09:33 -0300

    fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -20,11 +20,11 @@ export const AppDataSource = new DataSource(process.env.NODE_ENV === "test" ?
         logging: true,
         entities:
             process.env.NODE_ENV === "production"
-                ? ["dist/src/entities/*.js"]
+                ? ["dist/entities/*.js"]
                 : ["src/entities/*.ts"],
         migrations:
             process.env.NODE_ENV === "production"
-                ? ["dist/src/migrations/*.js"]
+                ? ["dist/migrations/*.js"]
                 : ["src/migrations/*.ts"],
     });
 
