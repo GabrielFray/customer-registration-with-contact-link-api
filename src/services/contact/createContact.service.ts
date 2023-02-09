68b0b1c598e089a0c8ecbdeaf3a99430d608c80c
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-02-09 13:25:30 -0300

    fix: Fixed bugs

--- src/services/contact/createContact.service.ts
+++ src/services/contact/createContact.service.ts
@@ -1,8 +1,8 @@
-import { AppDataSource } from "../../data-source";
-import { Contact } from "../../entities/contact.entity";
-import { User } from "../../entities/user.entity";
-import { AppError } from "../../errors/appError";
-import { IContactRequest } from "../../interfaces/contact";
+import { AppError } from "../../errors/appError"
+import { AppDataSource } from "../../data-source"
+import { User } from "../../entities/user.entity"
+import { Contact } from "../../entities/contact.entity"
+import { IContactRequest } from "../../interfaces/contact"
 
 export const createContactService = async ({ name, email, telephone, userId }: IContactRequest) => {
     const userRepository = AppDataSource.getRepository(User)
@@ -14,6 +14,10 @@ export const createContactService = async ({ name, email, telephone, userId }: I
 
     if (!findUser) {
         throw new AppError("User not found", 404)
+    }
+
+    if (contactAlreadyExist) {
+        throw new AppError("This email is already being used in another contact")
     }
 
     const contact = contactRepository.create({ name, email, telephone })
